9ba41a19e87acb83fd1b39f0184d88bfa7513dc8
Arun <arun14072001@gmail.com>
2025-04-18 20:11:52 +0530

    Initial Commit

---
+++ Steady App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Steady App
+//
+//  Created by Arun M on 18/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Steady App/Steady_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Steady_AppApp.swift
+//  Steady App
+//
+//  Created by Arun M on 18/04/25.
+//
+
+import SwiftUI
+
+@main
+struct Steady_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

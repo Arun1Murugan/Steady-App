9a88dcccaee38ec46b35aecc4e0532d21fbe780c
Arun <arun14072001@gmail.com>
2025-04-18 20:52:43 +0530

    Init MVVM arch

---
+++ Steady App/ViewModel/ChecklistViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ChecklistViewModel.swift
+//  Steady App
+//
+//  Created by Arun M on 18/04/25.
+//
+
+import Foundation
---
+++ Steady App/ViewModel/DataManager.swift
@@ -0,0 +1,8 @@
+//
+//  DataManager.swift
+//  Steady App
+//
+//  Created by Arun M on 18/04/25.
+//
+
+import Foundation
